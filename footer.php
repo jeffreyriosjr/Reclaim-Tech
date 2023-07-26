1eb1b36351a92ff00d5a3c89b4396b5614aa0b1d
jeffrey rios jr <jeffriosjr@gmail.com>
2023-07-25 21:33:19 -0700

    final touches

--- footer.php
+++ footer.php
@@ -11,24 +11,130 @@
 
 ?>
 
-	<footer id="colophon" class="site-footer">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'harrison-test' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'harrison-test' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'harrison-test' ), 'harrison-test', '<a href="http://underscores.me/">Underscores.me</a>' );
-				?>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
-</div><!-- #page -->
 
 <?php wp_footer(); ?>
 
-</body>
-</html>
+<div class="container-fluid">
+
+	<!-- Footer -->
+	<section class="text-center text-black m-2">
+		<!-- Section: Links  -->
+		<section class="resources" style="border: darkblue; border-width: 2px; border-style: solid;">
+			<div class="container-fluid text-center main-resources" style="font-size: 16px;">
+				<!-- Grid row -->
+				<div class="row mt-3 d-flex flex-direction-row justify-content-center">
+					<!-- Grid column -->
+					<div class="col-md-2 col-lg-2 col-xl-2 mx-auto mt-5">
+						<!-- Links -->
+						<h6 class="text-uppercase fw-bold">Links</h6>
+						<hr class="mb-4 mt-0 d-inline-block mx-auto"
+							style="width: 60px; background-color: #7c4dff; height: 2px" />
+						<p>
+							<a href="http://harrisontest.local/" class="text-black">Home</a>
+						</p>
+						<p>
+							<a href="http://harrisontest.local/contact/" class="text-black">Contact</a>
+						</p>
+						<p>
+							<a href="http://harrisontest.local/donations/" class="text-black">Donations</a>
+						</p>
+						<p>
+							<a href="http://harrisontest.local/services/" class="text-black">Services</a>
+						</p>
+						<p>
+							<a href="http://harrisontest.local/strfrnt//" class="text-black">STRFRNT</a>
+						</p>
+						<p>
+							<a href="http://harrisontest.local/team/" class="text-black">Team</a>
+						</p>
+					</div>
+					<!-- Grid column -->
+
+					<!-- Grid column -->
+					<div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mt-5">
+						<!-- Links -->
+						<h6 class="text-uppercase fw-bold">Contact</h6>
+						<hr class="mb-4 mt-0 d-inline-block mx-auto"
+							style="width: 60px; background-color: #7c4dff; height: 2px" />
+						<p><i class="fas fa-home mr-3"></i> 2600 Ventura Street, Fresno, California 93721</p>
+						<p><i class="fas fa-envelope mr-3"></i> Contact @
+							reclaimtechfresno@gmail.com</p>
+						<p><i class="fas fa-phone mr-3"></i> +1-555-555-5555</p>
+						<p><i class="fas fa-print mr-3"></i> Reclaim Technologies, Inc.</p>
+					</div>
+					<!-- Grid column -->
+
+					<!-- Grid column -->
+					<div class="col-md-3 col-lg-2 col-xl-2 mx-auto mt-5">
+						<!-- Links -->
+						<h6 class="text-uppercase fw-bold">Resources</h6>
+						<hr class="mb-4 mt-0 d-inline-block mx-auto"
+							style="width: 60px; background-color: #7c4dff; height: 2px" />
+						<p>
+							<a href="http://harrisontest.local/news-blog/">Latest Blogs</a>
+						</p>
+						<p>
+							<a href="http://harrisontest.local/news-blog//" class="text-black">Latest Articles</a>
+						</p>
+						<p>
+							<a href="http://harrisontest.local/news-blog/" class="text-black">Latest News</a>
+						</p>
+						<p>
+							<a href="http://harrisontest.local/news-blog/" class="text-black">Must Reads</a>
+						</p>
+					</div>
+					<!-- Grid column -->
+				</div>
+				<!-- Grid row -->
+			</div>
+		</section>
+		<!-- Section: Links  -->
+	</section>
+	<!-- Footer -->
+
+</div>
+<footer class="text-center text-lg-start text-muted the-footer">
+	<!-- Section: Links  -->
+	<section class="main-footer">
+		<div class="container-fluid text-center text-md-start">
+			<div class="text-center d-flex justify-content-center">
+				<!-- Grid column -->
+				<div class="col footer-logo">
+					<img class="nav-logo"
+						src="http://harrisontest.local/wp-content/uploads/2023/07/alex_treas_logo_R_for_technology_minimalist_91-e1689499300542.png"
+						alt="footer logo">
+				</div>
+			</div>
+
+		</div>
+	</section>
+	<!-- Section: Links  -->
+
+	<!-- Copyright -->
+	<div class="text-center" style="color: white;font-size: 14px;">
+		<ul class="icon-section">
+			<li class="icon-links">
+				<a href="https://www.facebook.com/SuzukiAssociatesInc/">
+					<i class="fab fa-facebook"></i></a>
+			</li>
+			<li class="icon-links">
+				<a href="https://www.instagram.com/suzuki_associates/">
+					<i class="fab fa-instagram"></i></a>
+			</li>
+			<li class="icon-links">
+				<a href="https://www.linkedin.com/in/jason-suzuki-3828a11b4/">
+					<i class="fab fa-linkedin"></i></a>
+			</li>
+		</ul>
+	</div>
+	<!-- Copyright -->
+	<div class="text-center p-3 mt-5">
+		© 2023 Copyright:
+		<p>
+			<a href="https://jeffreyriosjr.info/" class="text-black">jeffreyriosjr.info</a>
+		</p>
+
+	</div>
+	<!-- Copyright -->
+	<!-- Copyright -->
+</footer>
--- header.php
+++ header.php
@@ -12,48 +12,64 @@
 ?>
 <!doctype html>
 <html <?php language_attributes(); ?>>
+
 <head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta charset="<?php bloginfo('charset'); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+		integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+	<link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link
+		href="https://fonts.googleapis.com/css2?family=Lato&family=Montserrat&family=Oswald:wght@200&family=Qwitcher+Grypen&display=swap"
+		rel="stylesheet">
+	<script src="https://kit.fontawesome.com/ba414641d6.js" crossorigin="anonymous"></script>
 
 	<?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
-<?php wp_body_open(); ?>
-<div id="page" class="site">
-	<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'harrison-test' ); ?></a>
+	<?php wp_body_open(); ?>
+	<div id="page" class="site">
+		<a class="skip-link screen-reader-text" href="#primary">
+			<?php esc_html_e('Skip to content', 'harrison-test'); ?>
+		</a>
 
-	<header id="masthead" class="site-header">
-		<div class="site-branding">
-			<?php
-			the_custom_logo();
-			if ( is_front_page() && is_home() ) :
-				?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+		<header id="masthead" class="site-header">
+			<div class="site-branding">
 				<?php
-			else :
-				?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-				<?php
-			endif;
-			$harrison_test_description = get_bloginfo( 'description', 'display' );
-			if ( $harrison_test_description || is_customize_preview() ) :
-				?>
-				<p class="site-description"><?php echo $harrison_test_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
-			<?php endif; ?>
-		</div><!-- .site-branding -->
+				the_custom_logo();
+				if (is_front_page() && is_home()):
+					?>
+					<h1 class="site-title"><a href="<?php echo esc_url(home_url('/')); ?>" rel="home"><?php bloginfo('name'); ?></a></h1>
+					<?php
+				else:
+					?>
+					<p class="site-title"><a href="<?php echo esc_url(home_url('/')); ?>" rel="home"><?php bloginfo('name'); ?></a></p>
+					<?php
+				endif;
+				$harrison_test_description = get_bloginfo('description', 'display');
+				if ($harrison_test_description || is_customize_preview()):
+					?>
+					<p class="site-description">
+						<?php echo $harrison_test_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?>
+					</p>
+				<?php endif; ?>
+			</div><!-- .site-branding -->
 
-		<nav id="site-navigation" class="main-navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Start Here', 'harrison-test' ); ?></button>
-			<?php
-			wp_nav_menu(
-				array(
-					'theme_location' => 'menu-1',
-					'menu_id'        => 'primary-menu',
-				)
-			);
-			?>
-		</nav><!-- #site-navigation -->
-	</header><!-- #masthead -->
+			<nav id="site-navigation" class="main-navigation">
+				<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false">
+					<?php esc_html_e('Start Here', 'harrison-test'); ?>
+				</button>
+				<?php
+				wp_nav_menu(
+					array(
+						'theme_location' => 'menu-1',
+						'menu_id' => 'primary-menu',
+					)
+				);
+				?>
+			</nav><!-- #site-navigation -->
+		</header><!-- #masthead -->
